d620aa3635fab958e1ca27bf70ee19126fd213f4
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-08-02 19:46:22 -0300

    Desenvolve exemplo relativo a objetos, quarto dia do bloco 4.

--- exemplos.js
+++ exemplos.js
@@ -1,8 +1,3 @@
-/* let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 }; */
-
 let player = {
     name: 'Marta',
     lastName: 'Silva',
@@ -13,3 +8,22 @@ let player = {
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade");
 console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes.");
 console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.");
+
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+  for (let key in names){
+  console.log("Olá " + names[key]);
+  }
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+  for (let key in car){
+      console.log(key, car[key]);
+  }
